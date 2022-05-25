934604723f50affd17b1ed937fff7e9545468af9
Volodymyr-Bezyk <v.v.bezyk@gmail.com>
2022-05-25 20:25:12 +0300

    add mobile-menu

---
+++ js/mobile-menu.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openMenu: document.querySelector('[data-menu-open]'),
+    closeMenu: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
+
+  refs.openMenu.addEventListener('click', toggleMenu);
+  refs.closeMenu.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-rotate');
+  }
+})();
