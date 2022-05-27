aa79571d3bbea106c8f576e3231cc72217113a35
Volodymyr-Bezyk <v.v.bezyk@gmail.com>
2022-05-27 10:02:18 +0300

    mobile version finished

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,6 +3,7 @@
     openMenu: document.querySelector('[data-menu-open]'),
     closeMenu: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
+    body: document.querySelector('body'),
   };
 
   refs.openMenu.addEventListener('click', toggleMenu);
@@ -10,5 +11,6 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-rotate');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
